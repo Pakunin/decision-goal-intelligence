0a42123b5910b1324ce91ed9fbcd78b49c59b394
Aniruddha Dutta <dutta.aniruddha2006@gmail.com>
2026-04-12 01:14:21 +0530

    db setup

---
+++ database/setup.sql
@@ -0,0 +1,17 @@
+-- Create schemas
+\i schema/create_schemas.sql
+
+-- Create tables
+\i schema/create_tables.sql
+
+-- Constraints
+\i schema/constraints.sql
+
+-- Triggers
+\i schema/triggers.sql
+
+-- Views
+\i analytics/views.sql
+
+-- Sample data
+\i sample_data/sample_data.sql
